a8f80d2a05d1c0791b34666bf195bd3baf0f6cfe
jemiliolopez <jemiliolopezalvarez@gmail.com>
2019-12-07 22:58:42 +0100

    all

--- wordpress/shell.sh
+++ wordpress/shell.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-oc process -f build/web-wp-v2.json | oc apply -l 'app-service=web-mysql' -f -
+oc process -f build/web-wp-v2.json | oc apply -l 'build=web-wp -f -
+oc process -f build/web-wp-v2.json | oc delete -l 'build=web-wp -f -
 oc delete all --all -n image-wp
